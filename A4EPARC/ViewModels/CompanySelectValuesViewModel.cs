44dd1dd8a5602b395e530c88ad8342f9b6727f45
ianbeck76 <ianbeck76@live.com>
2015-04-04 15:40:31 +0100

    rest

---
+++ A4EPARC/ViewModels/CompanySelectValuesViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace A4EPARC.ViewModels
+{
+    public class CompanySelectValuesViewModel
+    {
+        public int Id { get; set; }
+        public int CompanyId { get; set; }
+        public int Key { get; set; }
+        public string Value { get; set; }
+    }
+}
